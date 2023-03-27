77043b4ba80ac1108abdb46b2f9223b845e47dee
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2023-03-27 21:30:17 +0200

    Up: -Cambio chiamata axios; -Aggiunta paginate; -card fixed; -header fixed

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -28,18 +28,32 @@ export default {
     },
 }
 </script>
+
 <template lang="">
+    <div class="container-fluid">
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="light">
+  <div class="container-fluid">
     <div>
-        <ul class="navbar-nav mr-auto">
-                    <li class="nav-item" v-for="(item, index) in menuItems" :key="index">
-<!--                             <a :href="item.routeName" class="nav-link">
-                                {{ item.label }}
-                            </a> -->
-                            <router-link :to="{ name: item.routeName }" class="nav-link">
-                                {{ item.label }}
-                            </router-link>
-                    </li>
-                </ul>
+        <a class="navbar-brand" href="#">Deliveboo</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+    <div class="collapse navbar-collapse flex-row-reverse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item" v-for="(item, index) in menuItems" :key="index">
+            <!--<a :href="item.routeName" class="nav-link">
+                {{ item.label }}
+            </a> -->
+            <router-link :to="{ name: item.routeName }" class="nav-link">
+                {{ item.label }}
+            </router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
     </div>
 </template>
 <style lang="scss" scoped></style>
--- src/pages/RestaurateurCard.vue
+++ src/pages/RestaurateurCard.vue
@@ -5,9 +5,9 @@ export default {
     props: {
         restaurateur: Object,
     },
-    data(){
-        return{
-            restaurateurs: [],
+    data() {
+        return {
+            restaurateurs: null,
             store,
         }
     },
@@ -18,18 +18,24 @@ export default {
 <template>
     <div>
         <div class="card-body">
-                                <div class="card-title">
-                                    <h5>{{ restaurateur.name }}</h5>
-                                </div>
-                                <router-link :to="{ name: 'Plates', params: { id: restaurateur.id } }" class="btn btn-sm btn-success">Leggi</router-link>
-                            </div>
+            <div class="card-img-top">
+                <img :src="restaurateur.image != null ? `${this.store.baseUrl}/storage/${restaurateur.image}` : 'https://picsum.photos/200/300'"
+                    alt="">
+            </div>
+            <div class="card-title">
+                <h5>{{ restaurateur.name }}</h5>
+            </div>
+            <p class="card-text">{{ restaurateur.address }}</p>
+            <router-link :to="{ name: 'Plates', params: { id: restaurateur.id } }"
+                class="btn btn-sm btn-success">Descrizione</router-link>
+        </div>
     </div>
 </template>
 
 
 <style lang="scss" scoped>
-    img{
-        width: 200px;
-        height: 300px;
-    }
+img {
+    width: 200px;
+    height: 300px;
+}
 </style>
