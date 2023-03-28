76059155bb78116ead29f621ef600ce7f0b53ea9
Leo9723 <pala9761@gmail.com>
2023-03-28 10:46:30 +0200

    pagina di visualizzazione piatti

--- src/pages/PlatesPage.vue
+++ src/pages/PlatesPage.vue
@@ -1,12 +1,42 @@
 <script>
+import axios from 'axios';
+import { store } from '../store';
+
 export default {
     name: "PlatesPage",
+    data(){
+        return{
+            store,
+            loading : true,
+            plates: [],
+        }
+    },
+    mounted(){
+        this.loading = true;
+        axios.get(`${this.store.baseUrl}/api/restaurateurs/${this.$route.params.id}`).then((response) => {
+            if(response.data.success){
+                console.log(response.data.plates)
+                this.plates = response.data.plates
+                this.loading = false
+            }
+            else{
+
+                this.$router.push({ name:'not-found' })
+
+            }
+        })
+    }
 }
 </script>
 
 <template lang="">
-    <div>
-        PAGINA PIATTI
+    <div v-for="plate in plates">
+       <div class="name">
+            {{ plate.name }}
+       </div>
+       <div class="price">
+            {{ plate.price }}
+       </div>
     </div>
 </template>
 
