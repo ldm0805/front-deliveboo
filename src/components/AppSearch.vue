ed1347379286787245a8a272304621f0004c2e49
Fra-Battaglia <francescobattaglia03@gmail.com>
2023-03-31 09:41:13 +0200

    searchbar style changed

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -19,19 +19,24 @@ export default {
     <div class="container">
         <div class="row">
             <div class="input-group">
-                <input type="text" id="search" class="form-control" placeholder="Ricerca il ristorante" aria-label="Ricerca il film" v-model="store.inputText" @keyup.enter="$emit('search')">
-                <button class="btn btn-danger" type="button" @click="searchMedia" >Cerca</button>
+                <input type="text" id="search" class="form-control border-light bg-transparent text-light" placeholder="Ricerca il ristorante" aria-label="Ricerca il film" v-model="store.inputText" @keyup.enter="$emit('search')">
+                <button class="btn btn-outline-light" type="button" @click="searchMedia" >Cerca</button>
             </div>
         </div>
     </div>
 </template>
 <style lang="scss" scoped>
-// @use '../styles/partials/variables' as *;
-// @use '../styles/partials/mixins' as *;
+    @use '../styles/partials/variables' as *;
+    @use '../styles/partials/mixins' as *;
 
-.form-control:focus {
-    border-color: #a50500;
-    outline: 0;
-    box-shadow: 0 0 0 0rem rgba(117, 0, 0, 0.25);
-}
+    .form-control:focus {
+        border-color: #a50500;
+        outline: 0;
+        box-shadow: 0 0 0 0rem rgba(117, 0, 0, 0.25);
+    }
+
+    .form-control::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
+    color: $light_color;
+    opacity: 1; /* Firefox */
+    }
 </style>
