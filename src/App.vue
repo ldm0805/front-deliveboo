f3159a2649ff0f5badcea3afccafd53ed62963b1
Fra-Battaglia <francescobattaglia03@gmail.com>
2023-03-31 09:34:59 +0200

    header size changed

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <script>
 import AppHeader from './components/AppHeader.vue'
 export default {
-  components: {
-    AppHeader
-  }
+	components: {
+		AppHeader
+	}
 }
 </script>
 <template lang="">
-  <AppHeader></AppHeader>
-  <router-view></router-view>
+	<AppHeader></AppHeader>
+	<router-view></router-view>
 </template>
 <style lang="scss">
-@use './styles/app.scss' as *;
+	@use './styles/app.scss' as *;
 </style>
