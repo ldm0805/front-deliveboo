80c444fa49927572a487d440dc87644e2eaeef2f
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2023-04-02 12:04:41 +0200

    pagamento fixed di base, ma da aggiungere dati

--- src/main.js
+++ src/main.js
@@ -3,18 +3,21 @@ import './style.css'
 import App from './App.vue'
 import { router } from './router'
 import "bootstrap/dist/css/bootstrap.min.css"
-var button = document.querySelector('#submit-button');
 
-braintree.dropin.create({
-    authorization: 'sandbox_mfb44vg7_y7bkdpct43k678pk',
-    selector: '#dropin-container'
-}, function (err, instance) {
-    button.addEventListener('click', function () {
-        instance.requestPaymentMethod(function (err, payload) {
-            // Submit payload.nonce to your server
-        });
-    })
+
+document.addEventListener('DOMContentLoaded', function () {
+    const button = document.querySelector('#submit-button');
+
+    braintree.dropin.create({
+        authorization: 'sandbox_mfb44vg7_y7bkdpct43k678pk',
+        selector: '#dropin-container'
+    }, function (err, instance) {
+        button.addEventListener('click', function () {
+            instance.requestPaymentMethod(function (err, payload) {
+                // Submit payload.nonce to your server
+            });
+        })
+    });
 });
-
 createApp(App).use(router).mount('#app')
 
