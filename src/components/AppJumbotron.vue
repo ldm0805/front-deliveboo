76dc1a3f1d31133311ea6157e6a8121618687d16
Fra-Battaglia <francescobattaglia03@gmail.com>
2023-04-07 15:58:58 +0200

    cards style done

--- src/components/AppJumbotron.vue
+++ src/components/AppJumbotron.vue
@@ -1,43 +1,42 @@
 <script>
-import axios from "axios";
-import AppSearch from "./AppSearch.vue";
-import { store } from "../store";
-import AppSelect from "./AppSelect.vue";
-export default {
-  name: "AppJumbotron",
-  components: {
-    AppSearch,
-    AppSelect,
-  },
+	import axios from "axios";
+	import AppSearch from "./AppSearch.vue";
+	import { store } from "../store";
+	import AppSelect from "./AppSelect.vue";
+	export default {
+		name: "AppJumbotron",
+		components: {
+			AppSearch,
+			AppSelect,
+		},
 
-  data() {
-    return {
-      store,
-    };
-  },
-};
+		data() {
+			return {
+				store,
+			};
+		},
+	};
 </script>
 <template lang="">
-  <section
-    class="jumbotron d-flex align-items-center justify-content-center my-text-light flex-column"
-  >
-    <div class="w-75 text-center animate__animated animate__fadeInDownBig">
-      <h1>Sì che hai fame</h1>
-      <AppSelect />
-    </div>
-  </section>
+	<section class="jumbotron d-flex align-items-center justify-content-center my-text-light flex-column">
+	<div class="w-75 text-center animate__animated animate__fadeInDownBig">
+		<h1>Sì che hai fame</h1>
+		<AppSelect />
+	</div>
+	</section>
 </template>
+
 <style lang="scss" scoped>
-@use "../styles/partials/variables" as *;
-.jumbotron {
-  height: 500px;
-  // background-image: url('../assets/img/food-table-1.jpg');
-  background-size: cover;
-  background-image: linear-gradient(
-      180deg,
-      rgba($color: $dark_color, $alpha: 0.3),
-      rgba($color: $dark_color, $alpha: 0.3)
-    ),
-    url("../assets/img/food-table-1.jpg");
-}
+	@use "../styles/partials/variables" as *;
+	.jumbotron {
+	height: 500px;
+	// background-image: url('../assets/img/food-table-1.jpg');
+	background-size: cover;
+	background-image: linear-gradient(
+		180deg,
+		rgba($color: $dark_color, $alpha: 0.3),
+		rgba($color: $dark_color, $alpha: 0.3)
+		),
+		url("../assets/img/food-table-1.jpg");
+	}
 </style>
--- src/pages/RestaurateurCard.vue
+++ src/pages/RestaurateurCard.vue
@@ -16,11 +16,8 @@ export default {
 <!-- Ciao sono un -->
 <template>
     <div class="card" v-if="store.selectedType == null">
-        <img class="card-img-top"
-            :src="restaurateur.image != null ? `${this.store.baseUrl}/storage/${restaurateur.image}` : 'https://picsum.photos/200/300'"
-            alt="">
-
-        <div class="card-body">
+        <img class="card__image" :src="restaurateur.image != null ? `${this.store.baseUrl}/storage/${restaurateur.image}` : 'https://picsum.photos/200/300'" alt="">
+        <div class="card__content">
             <div class="card-title">
                 <h5>Ristorante: {{ restaurateur.name }}</h5>
             </div>
@@ -32,16 +29,12 @@ export default {
                         :key="index">{{ tag.name }}</span>
                 </div>
             </div>
-            <router-link :to="{ name: 'Plates', params: { slug: restaurateur.slug } }" class="button">Vai ai
-                piatti</router-link>
+            <router-link :to="{ name: 'Plates', params: { slug: restaurateur.slug } }" class="button">Vai ai piatti</router-link>
         </div>
     </div>
     <div class="card" v-else-if="store.selectedType == ''">
-        <img class="card-img-top"
-            :src="restaurateur.image != null ? `${this.store.baseUrl}/storage/${restaurateur.image}` : 'https://picsum.photos/200/300'"
-            alt="">
-
-        <div class="card-body">
+        <img class="card__image" :src="restaurateur.image != null ? `${this.store.baseUrl}/storage/${restaurateur.image}` : 'https://picsum.photos/200/300'" alt="">
+        <div class="card__content">
             <div class="card-title">
                 <h5>Ristorante: {{ restaurateur.name }}</h5>
             </div>
@@ -53,16 +46,12 @@ export default {
                         :key="index">{{ tag.name }}</span>
                 </div>
             </div>
-            <router-link :to="{ name: 'Plates', params: { slug: restaurateur.slug } }" class="button">Vai ai
-                piatti</router-link>
+            <router-link :to="{ name: 'Plates', params: { slug: restaurateur.slug } }" class="button">Vai ai piatti</router-link>
         </div>
     </div>
     <div class="card" v-else>
-        <img class="card-img-top"
-            :src="restaurateur.image != null ? `${this.store.baseUrl}/storage/${restaurateur.image}` : 'https://picsum.photos/200/300'"
-            alt="">
-
-        <div class="card-body">
+        <img class="card__image" :src="restaurateur.image != null ? `${this.store.baseUrl}/storage/${restaurateur.image}` : 'https://picsum.photos/200/300'" alt="">
+        <div class="card__content">
             <div class="card-title">
                 <h5>Ristorante: {{ restaurateur.name }}</h5>
             </div>
@@ -90,15 +79,116 @@ export default {
     margin: .5em 0em;
     border-radius: .2em;
     text-decoration: none;
-    color: white;
+    color: #ffffff;
     display: inline-block;
 
     &:hover {
         background-color: #f86036;
     }
+    
+}
+.cart-link{
+    display: flex;
+    justify-content: center;
+    margin-top: 40px;
+}
 
-    .card-img-top {
-        height: 200px;
+h2,
+h4 {
+    color: var(--clr-head);
+}
+
+h2 {
+    font-size: var(--fs-h2);
+}
+
+.wrapper {
+    max-width: 1200px;
+    width: 100%;
+    margin: 3em auto;
+    padding: 0 2em;
+}
+
+.head-title {
+    text-align: center;
+}
+
+.head-text {
+    max-width: 520px;
+    margin: auto;
+    margin-top: 0.5em;
+}
+
+.cards {
+    max-width: 100%;
+    width: 1100px;
+    margin-top: 6em;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+    grid-gap: 6rem;
+}
+
+.card {
+    display: flex;
+    flex-direction: column;
+    border-radius: 13px;
+    overflow: hidden;
+    position: relative;
+    cursor: pointer;
+    transition: all 300ms ease-in-out;
+}
+
+.card:hover {
+    transform: translateY(-5px);
+    box-shadow: 10px 15px 50px 0 rgba(0, 0, 0, 0.09);
+}
+
+.card__image {
+    width: 100%;
+    aspect-ratio: 16 / 9;
+    object-fit: cover;
+    object-position: center;
+}
+
+.card__content {
+    padding: 2em 1.4em;
+    background-color: #fff;
+}
+
+.card__header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 1em;
+}
+
+.card__price {
+    font-size: 2.5rem;
+    color: var(--clr-accent);
+}
+
+.card__text {
+    font-size: 1.4rem;
+}
+.cart {
+    background-color: transparent;
+    border: 3px solid $primary_color;
+    border-radius: 10px;
+    padding: 5px 5px;
+    color: $primary_color;
+    font-weight: bold;
+    transition: all 0.25s;
+    display: flex;
+    align-items: center;
+
+    i{
+        margin: 5px 20px;
+        font-size: 25px;
+    }
+
+    &:hover {
+        background-color: $primary_color;
+        color: white;
     }
 }
 </style>
